acb5cd187a57a695c9b83e8698733db669acfda3
Deepak1122334455 <146498966+Deepak1122334455@users.noreply.github.com>
2024-09-23 21:40:07 +0530

    Update route_screen.dart

--- lib/screens/route_screen.dart
+++ lib/screens/route_screen.dart
@@ -1,1 +1,20 @@
+import 'package:flutter/material.dart';
 
+class RouteScreen extends StatelessWidget {
+  final Member member;
+
+  RouteScreen({required this.member});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('${member.name} - Route Traveled'),
+      ),
+      body: Center(
+        child: Text('Displaying route traveled for ${member.name}'),
+        // Integrate with Google Maps or another service to display the route.
+      ),
+    );
+  }
+}
